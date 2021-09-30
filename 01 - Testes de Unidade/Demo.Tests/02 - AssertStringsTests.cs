6829e28b4a4ff1f91302ef224ca63f6282a3ebbf
Igor Andrade Silva <igor.andrade1013@gmail.com>
2021-09-30 12:15:26 -0300

    Asserções (Assert)

---
+++ 01 - Testes de Unidade/Demo.Tests/02 - AssertStringsTests.cs
@@ -0,0 +1,92 @@
+using Xunit;
+
+namespace Demo.Tests
+{
+    public class AssertStringsTests
+    {
+        [Fact]
+        public void StringsTools_UnirNomes_RetornarNomeCompleto()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Igor", "Andrade");
+
+            // Assert
+            Assert.Equal("Igor Andrade", nomeCompleto);
+        }
+
+
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveIgnorarCase()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Igor", "Andrade");
+
+            // Assert
+            Assert.Equal("Igor Andrade", nomeCompleto, true);
+        }
+
+
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveConterTrecho()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Igor", "Andrade");
+
+            // Assert
+            Assert.Contains("or", nomeCompleto);
+        }
+
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveComecarCom()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Igor", "Andrade");
+
+            // Assert
+            Assert.StartsWith("Igo", nomeCompleto);
+        }
+
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveAcabarCom()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Igor", "Andrade");
+
+            // Assert
+            Assert.EndsWith("ade", nomeCompleto);
+        }
+
+
+        [Fact]
+        public void StringsTools_UnirNomes_ValidarExpressaoRegular()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Igor", "Andrade");
+
+            // Assert
+            Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", nomeCompleto);
+        }
+    }
+}
